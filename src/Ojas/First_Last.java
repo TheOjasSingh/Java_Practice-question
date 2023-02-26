e6f522840ad739e94668001001281b1809625767
Ojas Singh <ojas856@gmail.com>
2023-02-26 23:28:50 +0530

    Binary Search question added
    Signed-off-by: Ojas Singh <ojas856@gmail.com>

--- src/Ojas/First_Last.java
+++ src/Ojas/First_Last.java
@@ -10,18 +10,50 @@ public class First_Last {
 //        int[] result = search(arr, target);
         System.out.println(Arrays.toString(search(arr, target)));
     }
+    // Brute Force method
+//    static int[] search(int[] arr, int target){
+//        int[] ans = new int[] {-1, -1};
+//        for(int i =0;i<arr.length;i++){
+//            if(arr[i] == target){
+//                ans[0] = i;
+//                break;
+//            }
+//        }
+//        for(int i = arr.length-1;i>=0;i--){
+//            if(arr[i] == target){
+//                ans[1] = i;
+//                break;
+//            }
+//        }
+//        return ans;
+//    }
+
+    // Optimized method using Binary Search
     static int[] search(int[] arr, int target){
-        int[] ans = new int[] {-1, -1};
-        for(int i =0;i<arr.length;i++){
-            if(arr[i] == target){
-                ans[0] = i;
-                break;
-            }
-        }
-        for(int i = arr.length-1;i>=0;i--){
-            if(arr[i] == target){
-                ans[1] = i;
-                break;
+        int[] ans = {-1, -1};
+        int start = search1(arr, target, true);
+        int end = search1(arr, target, false);
+        ans[0] = start;
+        ans[1] = end;
+        return ans;
+    }
+    static int search1(int[] arr, int target, boolean findstartIndex){
+        int ans = -1;
+        int start = 0;
+        int end = arr.length -1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(target < arr[mid]){
+                end = mid -1;
+            }else if( target > arr[mid]){
+                start = mid + 1;
+            } else{
+                ans = mid;
+                if(findstartIndex){
+                    end = mid -1;
+                }else{
+                    start = mid + 1;
+                }
             }
         }
         return ans;
