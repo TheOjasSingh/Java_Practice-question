edfce024a3779bc3ccb1029299e4106b6986c123
Ojas Singh <ojas856@gmail.com>
2023-03-14 23:30:58 +0530

    Selection Sort
    Signed-off-by: Ojas Singh <ojas856@gmail.com>

--- src/Ojas/Bubble_Sort.java
+++ src/Ojas/Bubble_Sort.java
@@ -4,10 +4,33 @@ import java.util.Arrays;
 
 public class Bubble_Sort {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
-        bubble(arr);
+        int[] arr = {5,4,3,2,1};
+        selection(arr);
         System.out.println(Arrays.toString(arr));
     }
+    static void selection(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxindex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    private static int getMaxindex(int[] arr, int start, int last) {
+        int max = start;
+        for (int i = start; i <= last; i++) {
+            if(arr[max] < arr[i]){
+                max = i;
+            }
+        }
+        return max;
+    }
+
     static void bubble(int[] arr){
         boolean swapped;
         for (int i = 0; i < arr.length; i++) {
